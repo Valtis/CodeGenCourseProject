7efe2fd2f0791e787d36832af86e7c9965ba07b8
Valtis <erkka_kaaria@outlook.com>
2016-04-10 13:37:08 +0300

    ErrorHandling: Code cleanup, improved error messages

--- CodeGenCourseProject/ErrorHandling/ErrorData.cs
+++ CodeGenCourseProject/ErrorHandling/ErrorData.cs
@@ -10,9 +10,16 @@ namespace CodeGenCourseProject.ErrorHandling
     {
         private readonly Error type;
         private readonly string msg;
+
+        // line, column are zero-based internally
         private readonly int line;
         private readonly int column;
         private string[] lines;
+
+
+        private const ConsoleColor CODE_LINE_COLOR = ConsoleColor.White;
+        private const int DIRECTION_CHANGE_COLUMN_LENGTH = 10;
+        private const int LINE_LENGTH = 6;
 
         public Error Type
         {
@@ -58,10 +65,10 @@ namespace CodeGenCourseProject.ErrorHandling
 
         public void Print()
         {
-            ConsoleColor color = Console.ForegroundColor;
+            var color = Console.ForegroundColor;
             switch (Type)
             {
-                
+
                 case Error.NOTE_GENERIC:
                 case Error.NOTE:
                     color = ConsoleColor.Cyan;
@@ -99,54 +106,96 @@ namespace CodeGenCourseProject.ErrorHandling
 
 
             Console.ResetColor();
-            // line, column are zero-based internally
             Console.Write(msg);
+            
             if (type != Error.NOTE_GENERIC)
             {
                 Console.Write(" at line " + (line + 1) + " column " + (column + 1));
             }
+
             if (lines.Length == 0)
             {
                 return;
             }
 
             if (type != Error.NOTE_GENERIC)
-            { 
-                Console.Write("\n\n" + (line < lines.Length ? lines[line] : ""));
+            {
+                // Print the lines before and after error, as well as the error line and the ~~~~~ line
+                // Also naturally handle the case where the error is on the first or last line, and the
+                // previous or following line cannot be printed
 
-                Console.ForegroundColor = color;
-                if (column > 10)
-                {
-                    for (int i = 0; i < column - 6; ++i)
-                    {
-                        Console.Write(" ");
-                    }
-
-                    for (int i = 0; i < 6; ++i)
-                    {
-                        Console.Write("~");
-                    }
-                    Console.Write("^");
-
-                }
-                else
-                {
-                    for (int i = 0; i < column; ++i)
-                    {
-                        Console.Write(" ");
-                    }
-
-                    Console.Write("^");
-
-                    for (int i = 0; i < 6; ++i)
-                    {
-                        Console.Write("~");
-                    }
-                }
+                PrintPrecedingCodeLine();
+                PrintTheFocusLine();
+                PrintHighlightingLine(color);
+                PrintTrailingLine();
             }
-            Console.Write("\n");
+
             Console.ResetColor();
+            Console.Write("\n");
+
         }
 
+        private void PrintPrecedingCodeLine()
+        {
+            Console.ForegroundColor = CODE_LINE_COLOR;
+            if (line > 0)
+            {
+                Console.Write("\n\n" + (line < lines.Length ? lines[line - 1] : ""));
+            }
+            else
+            {
+                Console.Write("\n\n");
+            }
+        }
+
+        private void PrintTheFocusLine()
+        {
+            Console.ForegroundColor = CODE_LINE_COLOR;
+            Console.Write((line < lines.Length ? lines[line] : ""));
+        }              
+
+        private void PrintHighlightingLine(ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            if (column > DIRECTION_CHANGE_COLUMN_LENGTH)
+            {
+                for (int i = 0; i < column - LINE_LENGTH; ++i)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int i = 0; i < LINE_LENGTH; ++i)
+                {
+                    Console.Write("~");
+                }
+                Console.Write("^");
+
+            }
+            else
+            {
+                for (int i = 0; i < column; ++i)
+                {
+                    Console.Write(" ");
+                }
+
+                Console.Write("^");
+
+                for (int i = 0; i < LINE_LENGTH; ++i)
+                {
+                    Console.Write("~");
+                }
+            }
+        }
+
+        private void PrintTrailingLine()
+        {
+            Console.ForegroundColor = CODE_LINE_COLOR;
+
+            if (line + 1 <= lines.Length - 1)
+            {
+                Console.Write("\n" + (line < lines.Length ? lines[line + 1] : ""));
+
+            }
+        }
     }
 }
